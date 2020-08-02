b3f864a6779f71d176c534a4443f68dc839bc947
Mazin Abubeker <mazin@Mazins-MacBook-Pro.local>
2020-08-01 21:19:51 -0700

    yay

--- public/client.js
+++ public/client.js
@@ -3,12 +3,19 @@
 //   {Howl, Howler} = foo;
 // });
 
-var socket, sound;
+var socket;
+var sounds = [];
 var isPlaying = false;
+var songList = [];
+var allsongs;
+var song_num = 0;
 
 function readyRequest(){
   socket.emit('ready');
-  document.body.style.backgroundColor = "#d1d1d1";
+  document.getElementById('song-container').style.pointerEvents = "auto";
+  // document.getElementById('reset-btn').style.pointerEvents = "auto";
+  document.getElementById('play-btn').style.pointerEvents = "auto";
+  document.body.style.backgroundColor = "#ddd";
 }
 
 function playRequest(){
@@ -20,13 +27,23 @@ function reset(){
 }
 
 function stop(){
-  sound.stop();
+  sounds[song_num].stop();
+  document.getElementById('song-container').style.pointerEvents = "none";
+  // document.getElementById('reset-btn').style.pointerEvents = "none";
+  document.getElementById('play-btn').style.pointerEvents = "none";
   document.body.style.backgroundColor = "#fff";
+  song_num = 0;
+  allsongs[song_num].style.backgroundColor = "#ff928a";
+  for(var i = 0; i < allsongs.length; i++){
+    if(song_num!=i){
+      allsongs[i].style.backgroundColor = "#fff";
+    }
+  }
 }
 
 function play(timeToPlay){
   setTimeout(()=>{
-    sound.play();
+    sounds[song_num].play();
   }, timeToPlay - Date.now())
   document.body.style.backgroundColor = "#9dcff5";
 }
@@ -36,16 +53,51 @@ $(document).ready(function(){
   socket = io();
   socket.on('play_resp', play);
   socket.on('stop', stop);
-  
-  sound = new Howl({
-    src: ['assets/song2.mp3']
+  socket.on('song_list', list=>{
+    document.getElementById('song-container').innerHTML = '';
+    songList = list.items;
+    for(var i = 0; i < songList.length; i++){
+      sounds.push(new Howl({
+        src: ['assets/' + songList[i].split('$')[0] + '$' + songList[i].split('$')[1] + '.mp3']
+      }));
+    }
+    song_num = list.num;
+    console.log(songList);
+    for(var i = 0; i < songList.length; i++){
+      let title = songList[i].split('$')[0];
+      let artist = songList[i].split('$')[1];
+      let new_song_element = `<div class="song-item">` + title + `<span class="songname">` + artist + `</span></div>`;
+      document.getElementById('song-container').insertAdjacentHTML('beforeend', new_song_element);
+    }
+    allsongs = document.getElementsByClassName('song-item');
+    allsongs[0].addEventListener('click', ()=>{
+      socket.emit('request_song', 0);
+    });
+    allsongs[1].addEventListener('click', ()=>{
+      socket.emit('request_song', 1);
+    });
+    allsongs[2].addEventListener('click', ()=>{
+      socket.emit('request_song', 2);
+    });
+    allsongs[song_num].style.backgroundColor = "#ff928a";
   });
+  
+  socket.on('request_granted', num=>{
+    song_num = num;
+    console.log('wow');
+    allsongs[song_num].style.backgroundColor = "#ff928a";
+    for(var i = 0; i < allsongs.length; i++){
+      if(song_num!=i){
+        allsongs[i].style.backgroundColor = "#fff";
+      }
+    }
+  });
+
+  
+  
+  
 });
 
 
 // LAST THING - When server tells the client to play, add a 100ms delay and play after the difference
 //              of the current time and target time to sync.
-
-
-
-
--- server.js
+++ server.js
@@ -8,9 +8,18 @@ io.sockets.on('connection', newConnection)
 
 var isPlaying = false;
 var groupNum = 0;
+var song_num = 0;
+
+var fs = require('fs');
+var files = fs.readdirSync('public/assets/');
+var formatted_files = []
+for(var i = 0; i < files.length; i++){
+  formatted_files.push(files[i].substring(0,files[i].length-4));
+}
 
 function newConnection(socket){
   console.log("Connected user: " + socket.id);
+  socket.emit('song_list', {items: formatted_files, num: song_num});
   socket.on('play', ()=>{
     if(isPlaying){return;}
     isPlaying = true;
@@ -18,17 +27,19 @@ function newConnection(socket){
   });
 
   socket.on('ready', ()=>{
+    if(isPlaying){return;}
     socket.join(groupNum.toString());
   });
 
   socket.on('reset', ()=>{
     isPlaying = false;
-    io.in(groupNum.toString()).emit('stop');
-    groupNum++
-    // io.in('ready_users').leave('ready_users');
-    // Stop playing the song for "ready_users" in new function stop with document.body.bgnd color white
-    // clear the group "ready_users"
-    
+    io.emit('stop');
+    groupNum++;
+
   });
 
+  socket.on('request_song', (i)=>{
+    io.emit('request_granted', i);
+    song_num = i;
+  });
 }
